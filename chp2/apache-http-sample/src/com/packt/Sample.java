bd7b76c87f269bffb456106172933f7dcdc32294
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-11-13 12:02:30 +0300

    using jackson

--- chp2/apache-http-sample/src/com/packt/Sample.java
+++ chp2/apache-http-sample/src/com/packt/Sample.java
@@ -6,6 +6,11 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.http.util.EntityUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import com.packt.model.User;
+import java.net.URL;
+
 public class Sample{
 	public static void main (String [] args){
 		try(CloseableHttpClient client = HttpClients.createDefault()){
@@ -14,6 +19,15 @@ public class Sample{
 				System.out.println(response.getStatusLine());
 				HttpEntity entity = response.getEntity();
 				System.out.println(EntityUtils.toString(entity));
+
+				ObjectMapper mapper = new ObjectMapper();
+				List<User> users = mapper.readValue(new URL("http://jsonplaceholder.typicode.com/users"), 
+					new TypeReference<List<User>>(){});
+
+				for ( User user: users){
+					System.out.println("Name: " + user.name);
+				}
+
 			}catch(Exception ex){
 				ex.printStackTrace();
 			}
