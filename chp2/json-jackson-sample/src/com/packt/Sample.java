2e833e91ba6ef8d8aaa04a79c1b67f615fe89672
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-11-13 12:41:29 +0300

    removed apache jars

--- chp2/json-jackson-sample/src/com/packt/Sample.java
+++ chp2/json-jackson-sample/src/com/packt/Sample.java
@@ -1,10 +1,4 @@
 package com.packt;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.HttpEntity;
-import org.apache.http.util.EntityUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
