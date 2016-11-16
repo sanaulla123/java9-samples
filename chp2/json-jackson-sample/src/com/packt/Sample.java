d7ccce5a1a67aa0850b3a80d9583d4d5f614aceb
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-11-16 16:30:34 +0300

    trying to use internal JDK api

--- chp2/json-jackson-sample/src/com/packt/Sample.java
+++ chp2/json-jackson-sample/src/com/packt/Sample.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.packt.model.User;
 import java.net.URL;
 import com.fasterxml.jackson.core.type.TypeReference;
+import sun.reflect.Reflection;
 
 public class Sample{
 	public static void main (String [] args){
 		try{
+			System.out.println("Class: " + Reflection.getCallerClass(1).getName());
 			ObjectMapper mapper = new ObjectMapper();
 			List<User> users = mapper.readValue(new URL("http://jsonplaceholder.typicode.com/users"), 
 				new TypeReference<List<User>>(){});
