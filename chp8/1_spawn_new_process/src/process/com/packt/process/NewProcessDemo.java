6518cc86a3f1e4eb45fe1c86c53866f1021a50e4
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-12-20 01:04:40 +0300

    recipe for chapter 8

---
+++ chp8/1_spawn_new_process/src/process/com/packt/process/NewProcessDemo.java
@@ -0,0 +1,14 @@
+package com.packt.process;
+
+
+public class NewProcessDemo{
+	public static main void(String [] args){
+		ProcessBuilder pBuilder = new ProcessBuilder("top");
+		Process p = pBuilder.inheritIO().start();
+		if(p.waitFor(1, TimeUnit.SECONDS)){
+			System.out.println("process completed successfully");
+		}else{
+			System.out.println("waiting time elapsed, process did not complete");
+		}
+	}
+}
