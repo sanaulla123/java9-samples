5ee6bd6ab174a0b6f47d3979546bbc9c07b9e54e
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-12-22 20:37:35 +0300

    recipe 2 code

---
+++ chp8/2_redirect_to_file/src/process/com/packt/process/RedirectFileDemo.java
@@ -0,0 +1,25 @@
+package com.packt.process;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.Files;
+
+public class RedirectFileDemo{
+	public static void main(String[] args) throws IOException{
+		ProcessBuilder pb = new ProcessBuilder("iostat");
+		Process p = pb.redirectError(new File("error"))
+					  .redirectOutput(new File("output"))
+					  .start();
+		int exitValue = p.waitFor();
+
+		System.out.println("Error");
+		for(String line : Files.lines("error")){
+			System.out.println(line);
+		}
+
+		System.out.println("Output");
+		for(String line : Files.lines("output")){
+			System.out.println(line);
+		}
+	}
+}
