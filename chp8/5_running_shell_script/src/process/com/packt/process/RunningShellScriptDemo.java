961208afbd7db641a8e303b68d7b1e8c6689b195
Mohamed Sanaulla <sanaulla123@gmail.com>
2016-12-24 07:04:46 +0300

    some debuggnig

--- chp8/5_running_shell_script/src/process/com/packt/process/RunningShellScriptDemo.java
+++ chp8/5_running_shell_script/src/process/com/packt/process/RunningShellScriptDemo.java
@@ -8,9 +8,11 @@ public class RunningShellScriptDemo{
 		throws IOException, InterruptedException {
 
 		ProcessBuilder pb = new ProcessBuilder();
+		
+		pb.directory(new File("/root/java9-samples/chp8/5_running_shell_script"));
 
 		System.out.println(pb.directory());
-		
+
 		pb.command("script.sh").inheritIO();
 
 		Process p = pb.start();
