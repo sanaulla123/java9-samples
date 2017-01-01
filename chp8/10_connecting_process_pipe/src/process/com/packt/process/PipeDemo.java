65f75554fcfe6e4270e82c8326e96b27d36a4fcb
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-01-02 01:14:51 +0300

    code recipe 10

--- chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
+++ chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
@@ -1,7 +1,18 @@
 package com.packt.process;
-
+import java.io.*;
 public class PipeDemo{
 	public static void main(String[] args) throws Exception{
-		
+		List<ProcessBuilder> pipeline = List.of(
+			new ProcessBuilder("cat", "iris.data.txt"),
+			new ProcessBuilder("cut", "-d", ",", "-f", "5"),
+			new ProcessBuilder("uniq", "-c")
+		);
+		List<Process> processes = ProcessBuilder.startPipeline(pipeline);
+		Process lastProcess = processes.get(processes.size() - 1);
+		lastProcess.waitFor();
+		try(InputStream is = lastProcess.getInputStream();
+			BufferedReader r = new BufferedReader(new InputStreamReader(is))){
+			System.out.println(r.readLine());
+		}
 	}
 }
