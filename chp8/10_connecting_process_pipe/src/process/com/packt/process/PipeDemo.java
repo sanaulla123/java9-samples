88bbb89a5608b6bd383516091f5d1dbd255a6179
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-01-02 01:25:37 +0300

    code recipe 10

--- chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
+++ chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
@@ -7,7 +7,8 @@ public class PipeDemo{
 		List<ProcessBuilder> pipeline = List.of(
 			new ProcessBuilder("cat", "iris.data.txt"),
 			new ProcessBuilder("cut", "-d", ",", "-f", "5"),
-			new ProcessBuilder("uniq", "-c").redirectOutput(ProcessBuilder.Redirect.INHERIT)
+			new ProcessBuilder("uniq", "-c"),
+			new ProcessBuilder("sleep 60").redirectOutput(ProcessBuilder.Redirect.INHERIT)
 		);
 		List<Process> processes = ProcessBuilder.startPipeline(pipeline);
 		processes.stream().forEach( p -> {
