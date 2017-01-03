6afe182a49588abdfc308ed5553d67d2c5988734
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-01-03 23:55:50 +0300

    some changes

--- chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
+++ chp8/10_connecting_process_pipe/src/process/com/packt/process/PipeDemo.java
@@ -7,25 +7,12 @@ public class PipeDemo{
 		List<ProcessBuilder> pipeline = List.of(
 			new ProcessBuilder("cat", "iris.data.txt"),
 			new ProcessBuilder("cut", "-d", ",", "-f", "5"),
-			new ProcessBuilder("uniq", "-c"),
-			new ProcessBuilder("sleep", "2").redirectOutput(ProcessBuilder.Redirect.INHERIT)
+			new ProcessBuilder("uniq", "-c").redirectOutput(ProcessBuilder.Redirect.INHERIT)
 		);
+		
 		List<Process> processes = ProcessBuilder.startPipeline(pipeline);
-		processes.stream().forEach( p -> {
-			ProcessHandle.Info pInfo = p.info();
-			System.out.println(pInfo.command().orElse("command") + " " + p.isAlive());
-			if ( p.isAlive()) { 
-				try{
-					p.waitFor();
-				}catch(InterruptedException ex){
-					ex.printStackTrace();
-				}
-			}
-		});
-		//lastProcess.waitFor();
-		/*try(InputStream is = lastProcess.getInputStream();
-			BufferedReader r = new BufferedReader(new InputStreamReader(is))){
-			System.out.println(r.readLine());
-		}*/
+
+		int exitValue = processes.get(processes.size() - 1).waitFor();
+		
 	}
 }
