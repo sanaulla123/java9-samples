91ce8ff1b86a60866f5d76707c764a91681d52cc
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-07-29 03:23:03 +0300

    fixing issue

--- chp3/11_multirelease_jar/src/8/com/packt/FactoryDemo.java
+++ chp3/11_multirelease_jar/src/8/com/packt/FactoryDemo.java
@@ -4,7 +4,7 @@ package com.packt;
 public class FactoryDemo{
 	public static void main(String[] args){
 		System.out.println(CollectionUtil.list("element1", "element2", "element3"));
-		System.out.println(CollectionUtil.set("element1", "element2", "element3", "element1"));
+		System.out.println(CollectionUtil.set("element1", "element2", "element3"));
 	}
 
 }
