1548df24ce14bb706dafa5c81f8a0cdd0b51acc8
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-08-10 11:31:04 +0300

    fixing the issues in chp9 on linux

--- chp9/9_scatter_charts/run.sh
+++ chp9/9_scatter_charts/run.sh
@@ -1,3 +1,3 @@
-javac -p mlib\student.processor.jar -d mods --module-source-path src $(find src -name *.java)
-cp src\gui\com\packt\wickets mods\gui\com\packt\
+javac -d mods --module-source-path src $(find src -name *.java)
+cp src/gui/com/packt/wickets mods/gui/com/packt/
 java -p mods -m gui/com.packt.ScatterChartDemo
